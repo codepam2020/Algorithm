e805c9b51e71335aeb23667d1500228325630bfb
codePam <codepam2020@gmail.com>
2023-01-29 03:19:06 +0900

    Update test.py

--- Programmers/test.py
+++ Programmers/test.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 def solution(A,B):
     answer = []
 
@@ -10,3 +11,13 @@ def solution(A,B):
 ans = solution("3people !unFollowed me")
 
 print(ans)
+=======
+def solution(n):
+    answer = 0
+    return answer
+
+
+answer = solution(15)
+
+print(answer)
+>>>>>>> Stashed changes
