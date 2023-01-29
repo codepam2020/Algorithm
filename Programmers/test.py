107a2acedc93c151fa3940942f80fc79807ae9bb
codePam <codepam2020@gmail.com>
2023-01-29 11:38:00 +0900

    Update test.py

--- Programmers/test.py
+++ Programmers/test.py
@@ -1,6 +1,23 @@
-def solution(n):
-    answer = 0
-    return answer
+def solution(s):
+    word = s
+    ans = 0
+    while True:
+        afterWord = word
+        for i in range(1, len(afterWord)):
+            if afterWord[i - 1] == afterWord[i]:
+                afterWord = afterWord[: i - 1] + afterWord[i + 1 :]
+                break
+
+        if afterWord == word:
+            if afterWord == "":
+                ans = 1
+            else:
+                ans = 0
+            break
+
+        else:
+            word = afterWord
+    return ans
 
 
 answer = solution(15)
