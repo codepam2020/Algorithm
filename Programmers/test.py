2e2e90b401e72370fc5595e300375e3fae2f161a
codePam <codepam2020@gmail.com>
2023-02-05 01:07:00 +0900

    Update test.py

--- Programmers/test.py
+++ Programmers/test.py
@@ -1,4 +1,5 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 def solution(s):
     word = s
     ans = 0
@@ -8,12 +9,15 @@ def solution(s):
             if afterWord[i - 1] == afterWord[i]:
                 afterWord = afterWord[: i - 1] + afterWord[i + 1 :]
                 break
+=======
+def solution(n, words):
+    stack = [words[0]]
+    answer = []
+    for i in range(1, len(words)):
+>>>>>>> Stashed changes
 
-        if afterWord == word:
-            if afterWord == "":
-                ans = 1
-            else:
-                ans = 0
+        if stack[-1][-1] != words[i][0]:
+            answer.append(i + 1)
             break
 =======
 def solution(n, words):
@@ -22,13 +26,19 @@ def solution(n, words):
         print(i)
 >>>>>>> Stashed changes
 
-        else:
-            word = afterWord
-    return ans
+        elif words[i] in answer:
+            answer.append(i + 1)
+            break
+
+        stack.append(words[i])
+
+    return (answer[0] % n,)
 
 <<<<<<< Updated upstream
 
-answer = solution(15)
+answer = solution(
+    3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
+)
 
 print(answer)
 =======
